b6956a956317621f6fb0694afd0f63dc5c5222a3
Asish Sarangi <asishsarangi2005@gmail.com>
2026-03-11 20:35:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (85.66%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,14 @@
+#include<cmath>
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0 || x==1){
+            return x;
+        }
+        else {
+            return std::sqrt(x);
+
+        }
+        
+    }
+};
