bb11e9b06f9a5aa5532044c582658874cccab12e
Asish Sarangi <asishsarangi2005@gmail.com>
2026-03-12 08:31:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (81.32%) - LeetSync

--- 742-to-lower-case/to-lower-case.cpp
+++ 742-to-lower-case/to-lower-case.cpp
@@ -2,10 +2,15 @@ class Solution {
 public:
     string toLowerCase(string s) {
 
-        for(int i=0;i<s.length();i++){
-            s[i]=tolower(s[i]);
+        // for(int i=0;i<s.length();i++){
+        //     s[i]=tolower(s[i]);
+        // }
+        // return s;
+
+/*here ch refers to each character inside the string s, by changing ch it will change string s also, as it is referenced*/
+        for(char &ch : s) {   
+            ch = tolower(ch);
         }
-        return s;
-        
+        return s;   
     }
 };
