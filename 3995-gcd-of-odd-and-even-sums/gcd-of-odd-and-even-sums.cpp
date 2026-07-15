9fb67f70249daab98b0bfa5f1086aee03c03f1a3
Asish Sarangi <asishsarangi2005@gmail.com>
2026-07-15 21:02:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (9.11%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,7 +1,47 @@
 class Solution {
 public:
+    int gcd(int a, int b){
+        while(b != 0){
+            int temp = b;
+            b = a%b;
+            a = temp;
+
+        }
+        return a;
+    }
+
     int gcdOfOddEvenSums(int n) {
-        return n;
+        /* simply return n 
+        Explanation:-
+        sum of odd nos= 1+3+5+...+(2n-1)
+        sum of odd nos= n*n 
+        
+        sum of even nos= 2+4+6+...+2n
+        sum of even nos= 2(1+2+3+...+n)
+                        = 2(n(n+1)/2)
+                        = n(n+1)
+                        = n*n + n
+        therefore = GCD(n*n, n(n+1)) will be n
+        */
+
+        // return n;
+
+        int sumEven = 0;
+        int sumOdd = 0;
+        int odd=1;
+        for(int i=0;i<n;i++){
+            sumOdd = sumOdd+odd;
+            odd = odd+2;
+        }
+
+        int even=2;
+        for(int i=0;i<n;i++){
+            sumEven = sumEven+even;
+            even = even+2;
+        }
+        return gcd(sumOdd, sumEven);
+
+        
         
     }
 };
